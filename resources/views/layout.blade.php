6965da21d84ac3b7591db12c7ed64491d52a8116
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-01-24 01:57:14 +0100

    feat add web middleware to routes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <head>
     <title>Flat Pack - Admin Panel</title>
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     <link rel="stylesheet" href="{{ asset('flatpack/css/flatpack.css') }}" />
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Faustoq\Flatpack\Http\Controllers\HomeController;
 use Faustoq\Flatpack\Http\Controllers\ListController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{entity}/{id}', [FormController::class, 'index'])->name('flatpack.form');
-Route::get('/{entity}', [ListController::class, 'index'])->name('flatpack.list');
-Route::get('/', [HomeController::class, 'index'])->name('flatpack.home');
+Route::middleware(['web'])->group(function () {
+    Route::get('/{entity}/{id}', [FormController::class, 'index'])->name('flatpack.form');
+    Route::get('/{entity}', [ListController::class, 'index'])->name('flatpack.list');
+    Route::get('/', [HomeController::class, 'index'])->name('flatpack.home');
+});
