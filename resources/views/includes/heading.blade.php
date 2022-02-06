509f567a54a3299196d6bd5d1e2ad8b01377f956
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-02-06 21:12:50 +0100

    feat: editable fields

--- resources/views/includes/heading.blade.php
+++ resources/views/includes/heading.blade.php
@@ -2,7 +2,7 @@
 <label
     {{ $attributes->class([
         'w-full h-auto p-0 m-0 overflow-hidden',
-        'text-gray-800 border-0 bg-transparent outline-none focus:border-0 focus:bg-transparent focus:ring-0',
+        'text-gray-800 border-0 bg-transparent outline-none',
         'text-4xl font-bold' => $size === 'large',
         'text-xl font-normal' => $size === 'base' || $size === 'medium',
         'text-sm font-normal' => $size === 'small',
