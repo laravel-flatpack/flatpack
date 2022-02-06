2c19cb7b09a7c229279c013db500bcb85f6dc371
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-02-06 21:12:50 +0100

    feat: editable fields

--- resources/views/includes/editable.blade.php
+++ resources/views/includes/editable.blade.php
@@ -1,5 +1,65 @@
 @if (strtolower($type ?? '') === 'editable')
-<div>
-editable field
+<div x-cloak x-data="{ editing: false }" class="w-full max-w-6xl">
+    <button
+        x-show="!editing"
+        @click="editing=true; setTimeout(() => { $refs.editableInput.focus(); $refs.editableInput.select(); }, 10);"
+        {{ $attributes->class([
+            'editable-heading',
+            'w-full h-auto p-0 m-0 overflow-hidden',
+            'text-gray-800 bg-transparent outline-none',
+            'text-4xl font-bold' => $size === 'large',
+            'text-xl font-normal' => $size === 'base' || $size === 'medium',
+            'text-sm font-normal' => $size === 'small',
+        ]); }}
+    >
+        <span x-ref="editableText" class="input-field-placeholder {{ empty($value) ? 'opacity-70' : 'opacity-100' }}">
+            {{ empty($value) ? $placeholder : $value }}
+        </span>
+        <span class="edit-button">
+            <x-flatpack::icon icon="edit" size="small" />
+        </span>
+    </button>
+    <label
+        x-show="editing"
+        {{ $attributes->class([
+            'editable-heading',
+            'w-full h-auto p-0 m-0 overflow-hidden',
+            'text-gray-800 bg-transparent outline-none',
+            'text-4xl font-bold' => $size === 'large',
+            'text-xl font-normal' => $size === 'base' || $size === 'medium',
+            'text-sm font-normal' => $size === 'small',
+        ]); }}
+    >
+        <input
+            x-cloak
+            x-show="editing"
+            x-ref="editableInput"
+            @keyup.escape="editing=false; setTimeout(() => { $refs.editableInput.value = $refs.editableText.innerText; }, 10);"
+            @keyup.enter="editing=false; setTimeout(() => { $refs.editableText.innerText = $refs.editableInput.value; }, 10);"
+            @blur="editing=false; setTimeout(() => { $refs.editableText.innerText = $refs.editableInput.value; }, 10);"
+            wire:model.stop="fields.{{ $key }}"
+            wire:key="fields-{{ $key }}"
+            id="editable-{{ $key }}"
+            type="text"
+            name="{{ $key }}"
+            value="{{ $value }}"
+            {{ $attributes->class([
+                'form-field-input',
+                'w-full h-auto p-2',
+                'text-gray-800 bg-transparent',
+                'text-4xl font-bold' => $size === 'large',
+                'text-xl font-normal' => $size === 'base' || $size === 'medium',
+                'text-sm font-normal' => $size === 'small',
+            ]) }}
+        />
+        <span class="edit-button">
+            <div @click="editing=false; setTimeout(() => { $refs.editableInput.value = $refs.editableText.innerText; }, 10);">
+                <x-flatpack::icon icon="cancel" size="small" />
+            </div>
+            <div @click="editing=false; setTimeout(() => { $refs.editableText.innerText = $refs.editableInput.value; }, 10);">
+                <x-flatpack::icon icon="check" size="small" />
+            </div>
+        </span>
+    </label>
 </div>
 @endif
