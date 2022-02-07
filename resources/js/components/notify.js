edb99c0d3f85f36c55aabeb4444850a29ed7ea0f
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-02-07 21:45:18 +0100

    feat: toast notification

--- resources/js/components/notify.js
+++ resources/js/components/notify.js
@@ -1,37 +1,49 @@
-import { Notyf } from "notyf";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 
-const configuration = {
-  duration: 4000,
-  position: {
-    x: "center",
-    y: "bottom",
+const config = {
+  duration: 3000,
+  close: false,
+  gravity: "top",
+  position: "center",
+  stopOnFocus: true,
+  style: {
+    background: "#1f2937",
+    color: "#fff",
   },
-  types: [
-    {
-      type: "warning",
-      background: "#fbbf24",
-    },
-    {
-      type: "success",
-      background: "#22c55e",
-      duration: 2000,
-    },
-    {
-      type: "error",
-      background: "#f43f5e",
-    },
-    {
-      type: "info",
-      background: "#60a5fa",
-    },
-  ],
+  offset: {
+    y: "2rem",
+  },
 };
 
-const notify = ({ message, type = "info" }) => {
-  const notyf = new Notyf(configuration);
-  notyf.open({
-    type,
-    message,
-  });
+const notifications = {
+  info: (message) =>
+    Toastify({
+      ...config,
+      style: {
+        background: "#3b82f6",
+      },
+      text: message,
+    }).showToast(),
+
+  success: (message) =>
+    Toastify({
+      ...config,
+      style: {
+        background: "#22c55e",
+      },
+      text: message,
+    }).showToast(),
+  error: (message) =>
+    Toastify({
+      ...config,
+      style: {
+        background: "#ef4444",
+      },
+      text: message,
+    }).showToast(),
 };
+
+const notify = ({ message, type = "info" }) => notifications[type](message);
+
 export default notify;
