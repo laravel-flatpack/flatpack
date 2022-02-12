0c41450453665c80e936b362b1b0d0dc251e14ec
faustoq <faustoq@users.noreply.github.com>
2022-02-12 18:51:36 +0000

    fix: styling

--- tests/__snapshots__/posts-list.php
+++ tests/__snapshots__/posts-list.php
@@ -1,32 +1,32 @@
 <?php
 
-return array(
+return [
   'columns' =>
-  array(
+  [
     'id' =>
-    array(
+    [
       'label' => 'ID',
       'sortable' => true,
-    ),
+    ],
     'title' =>
-    array(
+    [
       'label' => 'Title',
       'sortable' => true,
       'searchable' => true,
-    ),
+    ],
     'created_at' =>
-    array(
+    [
       'label' => 'Created',
       'type' => 'datetime',
       'format' => 'M d, Y  h:i a',
       'sortable' => true,
-    ),
+    ],
     'updated_at' =>
-    array(
+    [
       'label' => 'Updated',
       'type' => 'datetime',
       'format' => 'M d, Y  h:i a',
       'sortable' => true,
-    ),
-  ),
-);
+    ],
+  ],
+];
