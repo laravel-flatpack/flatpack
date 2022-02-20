8a088819de9e3cf2d84c0877bf5649cc7e5d2635
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-02-20 22:25:58 +0100

    feat: custom alert and confirm dialogs

--- resources/views/components/action-button.blade.php
+++ resources/views/components/action-button.blade.php
@@ -1,21 +1,24 @@
-<div x-data="{ modalOpen: false }">
-@if ($confirm)
-    @include('flatpack::includes.modal-confirm')
-@endif
-    <button
-        @if ($confirm)
-        @click="modalOpen = !modalOpen"
-        @else
-        wire:loading.attr="disabled"
-        wire:click="{{ $method }}('{{ $action }}', @js($options))"
-        @endif
-        wire:key="{{ $key }}"
-        {{ $attributes->class([
-            'button text-lg font-medium lg:text-base lg:font-normal',
-            $style,
-            'hidden' => $hidden,
-        ]) }}
-    >
-        <span class="whitespace-nowrap">{{ $label }}</span>
-    </button>
-</div>
+<button
+    wire:key="{{ $key }}"
+    {{ $attributes->class([
+        'button text-lg font-medium lg:text-base lg:font-normal',
+        $style,
+        'hidden' => $hidden,
+    ]) }}
+    @if ($confirm)
+    @click="event.stopImmediatePropagation(); Flatpack.confirm(@js($confirmationMessage), {
+        title: @js($label),
+        action: @js($label),
+        style: @js($style)
+    }).then(({ isConfirmed }) => isConfirmed && @this.{{ $method }}('{{ $action }}', @js($options)));"
+    @else
+    wire:loading.attr="disabled"
+    wire:click="{{ $method }}('{{ $action }}', @js($options))"
+    @endif
+    @if(isset($options['shortcut']))
+        @keydown.window.prevent.cmd.{{ $options['shortcut'] }}="@this.{{ $method }}('{{ $action }}', @js($options))"
+        @keydown.window.prevent.ctrl.{{ $options['shortcut'] }}="@this.{{ $method }}('{{ $action }}', @js($options))"
+    @endif
+>
+    <span class="whitespace-nowrap">{{ $label }}</span>
+</button>
