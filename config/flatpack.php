daa269c6f9cb76be8aaa626e81b7512f24ed3528
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-03-05 14:55:16 +0100

    feat: custom brand

--- config/flatpack.php
+++ config/flatpack.php
@@ -6,6 +6,41 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Actions
+    |--------------------------------------------------------------------------
+    |
+    | Flatpack form or list actions. You can add your own actions here.
+    |
+    */
+    'actions' => [
+
+        'create' => \Faustoq\Flatpack\Actions\Create::class,
+
+        'delete' => \Faustoq\Flatpack\Actions\Delete::class,
+
+        'save' => \Faustoq\Flatpack\Actions\Save::class,
+
+        'upload' => \Faustoq\Flatpack\Actions\Upload::class,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Branding
+    |--------------------------------------------------------------------------
+    |
+    | The image and text to be displayed as the top element of the sidebar.
+    | Logo should be an absolute url or a relative path to the public directory.
+    |
+    */
+    'brand' => [
+
+        'name' => 'Flatpack',
+
+        'logo' => 'flatpack/images/logo.svg',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | Directory
     |--------------------------------------------------------------------------
     |
@@ -45,24 +80,7 @@ return [
         ]
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Actions
-    |--------------------------------------------------------------------------
-    |
-    | Flatpack form or list actions.
-    |
-    */
-    'actions' => [
 
-        'create' => \Faustoq\Flatpack\Actions\Create::class,
-
-        'delete' => \Faustoq\Flatpack\Actions\Delete::class,
-
-        'save' => \Faustoq\Flatpack\Actions\Save::class,
-
-        'upload' => \Faustoq\Flatpack\Actions\Upload::class,
-    ],
 
     'storage' => [
         'disk' => 'public',
