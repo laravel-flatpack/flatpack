ff8ce222b9184fd9018b6c4f295d753564e71db1
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-03-15 23:04:41 +0100

    fix: form field wrapping

--- resources/views/includes/form-fields.blade.php
+++ resources/views/includes/form-fields.blade.php
@@ -1,30 +1,27 @@
 <div>
-@foreach (groupComposition($fields) as $group)
-    @foreach ($group as $key => $options)
+@foreach (groupComposition($fields) as $groups)
+@foreach ($groups as $key => $options)
+    @if ($key === 'tabs')
+        @include('flatpack::includes.tabs', [
+            'options' => $options,
+            'formErrors' => $formErrors
+        ])
+    @else
+        @if ($loop->first)
+            <div x-data="{ open: true }" class="box">
 
-        @if ($key === 'tabs')
+                @if (getOption($options, 'group') !== null)
+                    <div class="flex items-center justify-between text-gray-500 cursor-pointer" @click="open = !open">
+                        <div class="text-xs font-bold uppercase">{{ getOption($options, 'group') }}</div>
+                        <span x-cloak x-show="!open"><x-flatpack::icon icon="expand_more" size="small" /></span>
+                        <span x-cloak x-show="open"><x-flatpack::icon icon="expand_less" size="small" /></span>
+                    </div>
+                @endif
 
-            @include('flatpack::includes.tabs', [
-                'options' => $options,
-                'formErrors' => $formErrors
-            ])
+                <div x-show="open" class="{{ $fieldset }}">
+        @endif
 
-        @else
-
-            @if ($loop->first)
-                <div x-data="{ open: true }" class="box">
-            @endif
-
-            @if ($loop->first && getOption($options, 'group') !== null)
-                <div class="flex items-center justify-between text-gray-500 cursor-pointer" @click="open = !open">
-                    <div class="text-xs font-bold uppercase">{{ getOption($options, 'group') }}</div>
-                    <span x-cloak x-show="!open"><x-flatpack::icon icon="expand_more" size="small" /></span>
-                    <span x-cloak x-show="open"><x-flatpack::icon icon="expand_less" size="small" /></span>
-                </div>
-            @endif
-
-            <div x-show="open" class="{{ $fieldset }}">
-                <x-flatpack-form-field
+            <x-flatpack-form-field
                     :key="$key"
                     :options="$options"
                     :entity="$entity"
@@ -32,14 +29,12 @@
                     :entry="$entry"
                     :fieldErrors="getErrors($formErrors, $key)"
                 />
-            </div>
 
-            @if($loop->last)
+        @if($loop->last)
                 </div>
-            @endif
-
+            </div>
         @endif
-
-    @endforeach
+    @endif
+@endforeach
 @endforeach
 </div>
