bb71328d50890468dd3a48146f99b194d6f9f267
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-03-17 20:06:03 +0100

    feat: relation field

--- src/Traits/WithRelationships.php
+++ src/Traits/WithRelationships.php
@@ -21,47 +21,6 @@ trait WithRelationships
         return ($this->isRelationship($key)) ? $this->entry->{$key}() : null;
     }
 
-    protected function getRelationshipModel($key)
-    {
-        $relation = $this->relation($key);
-
-        if (! $relation) {
-            return null;
-        }
-
-        return $relation->getRelated()::class;
-    }
-
-    /**
-     * Get the items to populate relationship select.
-     *
-     * @param string $key - the relationship key.
-     * @param string $display - the display column
-     * @return array
-     */
-    protected function getRelationshipItems($key, $display = 'name')
-    {
-        $values = [];
-
-        $relation = $this->relation($key);
-
-        if ($this->getRelationshipType($key) === 'belongsToMany') {
-            $values = $relation->getRelated()->get()->pluck(
-                $display,
-                $relation->getRelatedKeyName()
-            );
-        }
-
-        if ($this->getRelationshipType($key) === 'belongsTo') {
-            $values = $relation->getRelated()->get()->pluck(
-                $display,
-                $relation->getOwnerKeyName()
-            );
-        }
-
-        return $values;
-    }
-
     /**
      * Create a new related model instance.
      *
@@ -106,54 +65,6 @@ trait WithRelationships
         } else {
             $this->entry->{$key}()->sync($this->fields[$key]);
         }
-    }
-
-    /**
-     * Get the relationship type.
-     *
-     * @return string
-     */
-    protected function getRelationshipType($field)
-    {
-        $relation = $this->relation($field);
-
-        if ($relation instanceof \Illuminate\Database\Eloquent\Relations\BelongsTo) {
-            return 'belongsTo';
-        }
-
-        if ($relation instanceof \Illuminate\Database\Eloquent\Relations\BelongsToMany) {
-            return 'belongsToMany';
-        }
-
-        if ($relation instanceof \Illuminate\Database\Eloquent\Relations\HasMany) {
-            return 'hasMany';
-        }
-
-        if ($relation instanceof \Illuminate\Database\Eloquent\Relations\HasOne) {
-            return 'hasOne';
-        }
-
-        if ($relation instanceof \Illuminate\Database\Eloquent\Relations\MorphMany) {
-            return 'morphMany';
-        }
-
-        if ($relation instanceof \Illuminate\Database\Eloquent\Relations\MorphOne) {
-            return 'morphOne';
-        }
-
-        if ($relation instanceof \Illuminate\Database\Eloquent\Relations\MorphTo) {
-            return 'morphTo';
-        }
-
-        if ($relation instanceof \Illuminate\Database\Eloquent\Relations\MorphToMany) {
-            return 'morphToMany';
-        }
-
-        if ($relation instanceof \Illuminate\Database\Eloquent\Relations\BelongsToMany) {
-            return 'belongsToMany';
-        }
-
-        return 'unknown';
     }
 
     /**
