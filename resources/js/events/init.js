b29f97184eb8581febda226296cf9bce0ee8c78f
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-03-18 18:16:19 +0100

    feat: confirm dialog for unsaved form changes

--- resources/js/events/init.js
+++ resources/js/events/init.js
@@ -5,6 +5,17 @@ const initEventListeners = () => {
   Object.values(FLATPACK_EVENT).forEach((event) => {
     window.livewire.on(event, eventListeners[event]);
   });
+
+  window.addEventListener("beforeunload", function (event) {
+    event.preventDefault();
+    const form = document.querySelector("#form");
+    if (form && form.classList.contains("has-unsaved-changes")) {
+      event.returnValue =
+        "You have unsaved changes. Are you sure you want to leave?";
+    } else {
+      delete event["returnValue"];
+    }
+  });
 };
 
 export default initEventListeners;
