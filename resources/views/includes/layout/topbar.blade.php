a545e7eb311a0975f1240f494299d76b3f7b76f4
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-04-10 18:22:48 +0200

    fix: aria labels

--- resources/views/includes/layout/topbar.blade.php
+++ resources/views/includes/layout/topbar.blade.php
@@ -1,6 +1,6 @@
 <div class="top-bar-wrapper">
     <div class="flex items-center justify-start gap-5 text-gray-300">
-        <button @click="navbar = !navbar">
+        <button @click="navbar = !navbar" aria-label="{{ __('Toggle Navbar') }}">
             <div x-show="!navbar"><x-icon name="login" style="solid" class="w-6 h-6" /></div>
             <div x-show="navbar"><x-icon name="menu" style="outline" class="w-6 h-6" /></div>
         </button>
@@ -8,7 +8,7 @@
     <div class="flex gap-4 text-white">
         @auth
         <div x-data="{open: false}" class="relative">
-            <button @click="open = !open" aria-expanded="true" aria-haspopup="true" class="flex items-center justify-center gap-2 text-sm text-gray-400 hover:text-white">
+            <button @click="open = !open" aria-label="{{ __('User Options') }}" aria-expanded="true" aria-haspopup="true" class="flex items-center justify-center gap-2 text-sm text-gray-400 hover:text-white">
                 <x-icon name="user-circle" style="outline" class="w-6 h-6" />
             </button>
             <div x-show="open" @click.away="open = false" class="absolute right-0 w-40 mt-2 text-left origin-top-right bg-white rounded-md shadow-lg top-8 ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
