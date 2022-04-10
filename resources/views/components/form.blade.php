39c5d400564c41dbbdcd9341cae4bd707597eaab
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-04-10 15:35:21 +0200

    fix: layout

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,19 +1,55 @@
-<div x-cloak x-data id="form" class="flex flex-col w-full min-h-screen {{ $hasChanges ? 'has-unsaved-changes' : 'is-saved' }}">
+<div x-cloak x-data id="form" class="block w-full min-h-screen {{ $hasChanges ? 'has-unsaved-changes' : 'is-saved' }}">
 
-    <div class="flex flex-col-reverse items-start justify-start w-full gap-5 my-10 lg:justify-between lg:flex-row">
-        @include('flatpack::includes.form-header', [
-            'fieldset' => 'header-fieldset',
-            'elements' => $header,
-            'fields' => $fields,
-        ])
-        @include('flatpack::includes.toolbar', [
-            'elements' => $toolbar,
-            'entity' => $entity,
-            'model' => $model,
-        ])
+    <div class="grid grid-flow-row-dense grid-cols-12 gap-5 py-10">
+
+        <div class="col-span-12 md:col-span-7 lg:col-span-8 xl:col-span-9">
+            @include('flatpack::includes.form-header', [
+                'fieldset' => 'header-fieldset',
+                'elements' => $header,
+                'fields' => $fields,
+            ])
+        </div>
+
+        <div class="col-span-12 md:col-span-5 lg:col-span-4 xl:col-span-3">
+            @include('flatpack::includes.toolbar', [
+                'elements' => $toolbar,
+                'entity' => $entity,
+                'model' => $model,
+            ])
+        </div>
+
     </div>
 
-    <div class="flex flex-col w-full gap-6 lg:flex-row">
+    @if (count($sidebar) > 0)
+    <div class="grid grid-flow-row-dense grid-cols-12 gap-5">
+
+        <div class="col-span-12 lg:col-span-8 xl:col-span-9">
+            <div class="form-main">
+                @include('flatpack::includes.form-fields', [
+                    'fieldset' => 'form-fieldset',
+                    'fields' => $main,
+                    'entity' => $entity,
+                    'model' => $model,
+                ])
+            </div>
+        </div>
+
+        <div class="col-span-12 lg:col-span-4 xl:col-span-3">
+            <div class="form-sidebar">
+                @include('flatpack::includes.form-fields', [
+                    'fieldset' => 'sidebar-fieldset',
+                    'fields' => $sidebar,
+                    'entity' => $entity,
+                    'model' => $model,
+                ])
+            </div>
+        </div>
+
+    </div>
+
+    @else
+
+    <div class="w-full max-w-3xl mx-auto">
         <div class="form-main">
             @include('flatpack::includes.form-fields', [
                 'fieldset' => 'form-fieldset',
@@ -22,16 +58,8 @@
                 'model' => $model,
             ])
         </div>
-        @if (isset($sidebar) && count($sidebar) > 0)
-        <div class="form-sidebar">
-            @include('flatpack::includes.form-fields', [
-                'fieldset' => 'sidebar-fieldset',
-                'fields' => $sidebar,
-                'entity' => $entity,
-                'model' => $model,
-            ])
-        </div>
-        @endif
     </div>
+
+    @endif
 
 </div>
