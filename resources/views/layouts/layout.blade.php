96360547071c220ee43cfd23b867e0fd9eb3cf2c
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-04-10 15:35:21 +0200

    fix: layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -26,7 +26,7 @@
 
             @include('flatpack::includes.layout.topbar')
 
-            <div class="container px-5 pb-20 mx-auto max-w-[1440px]">
+            <div class="container px-5 pb-20 mx-auto max-w-[1330px]">
                 {{ $slot }}
             </div>
         </main>
