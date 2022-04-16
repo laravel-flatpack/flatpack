5b8a11e4103998804d7b94b5985f418f6e8b9d01
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-04-16 19:14:12 +0200

    feat: form input fields binding

--- src/Traits/WithFormFields.php
+++ src/Traits/WithFormFields.php
@@ -7,16 +7,50 @@ use Illuminate\Support\Str;
 trait WithFormFields
 {
     /**
+     * Form fields composition.
+     *
+     * @var array
+     */
+    public $formFields = [];
+
+    /**
+     * Form fields values.
+     *
+     * @var array
+     */
+    public $fields = [];
+
+    /**
+    * Model instance.
+    *
+    * @var \Illuminate\Database\Eloquent\Model
+    */
+    public $entry;
+
+    /**
+     * Field types excluded from validation and binding.
+     *
+     * @var array
+     */
+    protected $excludedTypes = [
+        "actions",
+        "toolbar",
+        "button",
+        "label",
+        "heading",
+        "divider",
+        "html"
+    ];
+
+    /**
      * Bind model to fields.
      *
      * @return void
      */
     protected function bindModelToFields()
     {
-        foreach ($this->getFormFields() as $key => $options) {
-            if ($this->entry->$key instanceof \Illuminate\Support\Carbon) {
-                $this->fields[$key] = $this->entry->$key->format('Y-m-d\TH:i:s');
-            } elseif ($this->entry->$key instanceof \Illuminate\Database\Eloquent\Collection) {
+        foreach ($this->onlyInputFields() as $key => $options) {
+            if ($this->entry->$key instanceof \Illuminate\Database\Eloquent\Collection) {
                 $id = optional($this->relation($key))->getRelatedKeyName();
                 $this->fields[$key] = $this->entry->$key->pluck($id)->toArray();
             } elseif ($this->entry->$key instanceof \Illuminate\Database\Eloquent\Model) {
@@ -34,13 +68,10 @@ trait WithFormFields
      */
     protected function bindFieldsToModel()
     {
-        $fields = $this->getFormFields();
-
-        foreach ($fields as $key => $options) {
-            if ((isset($options['type']) && in_array($options['type'], $this->excludedTypes)) ||
-                (isset($options['disabled']) && $options['disabled'] === true) ||
-                (isset($options['readonly']) && $options['readonly'] === true) ||
-                Str::contains($key, '_confirmation') ||
+        foreach ($this->onlyInputFields() as $key => $options) {
+            if (data_get($options, "disabled") === true ||
+                data_get($options, "readonly") === true ||
+                Str::contains($key, "_confirmation") ||
                 $this->isRelationship($key) ||
                 $this->fields[$key] === null) {
                 continue;
@@ -48,5 +79,19 @@ trait WithFormFields
 
             $this->entry->{$key} = $this->fields[$key];
         }
+    }
+
+    /**
+     * Get form fields composition, excluding the field types defined in $excludedTypes.
+     *
+     * @return array
+     */
+    protected function onlyInputFields()
+    {
+        return collect($this->formFields)
+            ->filter(function ($options) {
+                return !in_array(data_get($options, "type"), $this->excludedTypes);
+            })
+            ->toArray();
     }
 }
