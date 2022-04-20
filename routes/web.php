8a04c2a2ee0f497580721b2899561fe257a9180a
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-04-20 21:09:41 +0200

    fix: route grouping

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,20 @@ use Flatpack\Http\Controllers\ListController;
 use Flatpack\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('flatpack.login');
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('flatpack.login.action');
-});
+Route::name('flatpack.')->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+        Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login.action');
+    });
 
-Route::middleware('flatpack-auth')->group(function () {
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('flatpack.logout');
+    Route::middleware('flatpack-auth')->group(function () {
+        Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
-    Route::middleware('flatpack')->group(function () {
-        Route::post('/{entity}/{id}/upload', [UploadController::class, 'store'])->name('flatpack.upload');
-        Route::get('/{entity}/{id}', [FormController::class, 'index'])->name('flatpack.form');
-        Route::get('/{entity}', [ListController::class, 'index'])->name('flatpack.list');
-        Route::get('/', [HomeController::class, 'index'])->name('flatpack.home');
+        Route::middleware('flatpack')->group(function () {
+            Route::post('/{entity}/{id}/upload', [UploadController::class, 'store'])->name('upload');
+            Route::get('/{entity}/{id}', [FormController::class, 'index'])->name('form');
+            Route::get('/{entity}', [ListController::class, 'index'])->name('list');
+            Route::get('/', [HomeController::class, 'index'])->name('home');
+        });
     });
 });
