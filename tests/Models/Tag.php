91a56f20f1e9f26f843f769638d2fd59dc17db9e
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-04-25 22:16:57 +0200

    test: relationship traits

---
+++ tests/Models/Tag.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Flatpack\Tests\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tags';
+
+    /**
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'name'
+    ];
+}
---
+++ tests/Traits/WithRelationshipsTest.php
@@ -0,0 +1,82 @@
+<?php
+
+use Flatpack\Traits\WithRelationships;
+
+beforeEach(function () {
+    $this->trait = new class {
+        use WithRelationships {
+            relation as public;
+            createRelationship as public;
+            syncRelationship as public;
+            isRelationship as public;
+        }
+
+        public $formFields = [];
+        public $fields = [];
+        public $entry;
+
+        public function __construct()
+        {
+            $this->entry = new \Flatpack\Tests\Models\Post();
+
+            $this->formFields = [
+                'title' => [
+                    'label' => 'Title',
+                    'type' => 'text',
+                    'placeholder' => 'Enter a title',
+                    'rules' => 'required|string',
+                ],
+                'body' => [
+                    'label' => 'Body',
+                    'type' => 'editor',
+                    'rules' => 'required|string',
+                ],
+                'category' => [
+                    'label' => 'Category',
+                    'type' => 'relation',
+                    'relation' => [
+                        'name' => 'category',
+                        'display' => 'name'
+                    ]
+                ],
+                'tags' => [
+                    'label' => 'Tags',
+                    'type' => 'relation',
+                    'relation' => [
+                        'name' => 'tags',
+                        'display' => 'name'
+                    ]
+                ],
+                'created_at' => [
+                    'label' => 'Created at',
+                    'disabled' => true,
+                    'type' => 'datetime-picker'
+                ],
+                'updated_at' => [
+                    'label' => 'Updated at',
+                    'disabled' => true,
+                    'type' => 'datetime-picker'
+                ],
+            ];
+        }
+    };
+});
+
+it('returns the field relationship', function () {
+    $this->assertEquals(
+        $this->trait->relation('category'),
+        $this->trait->entry->category()
+    );
+});
+
+it('checks if the field is a valid relationship', function () {
+    $this->expect($this->trait->isRelationship('category'))
+        ->toBe(true);
+
+    $this->expect($this->trait->isRelationship('title'))
+        ->toBe(false);
+
+    $this->trait->entry = null;
+    $this->expect($this->trait->isRelationship('category'))
+        ->toBe(false);
+});
