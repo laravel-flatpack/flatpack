f1564387576ad6d8f2de5979316d409debefaaec
Fausto Quaggia <fausto.quaggia@gmail.com>
2022-10-05 23:28:28 +0200

    fix: default parse date format

--- resources/views/includes/form-field/date-picker.blade.php
+++ resources/views/includes/form-field/date-picker.blade.php
@@ -1,13 +1,14 @@
 @if (strtolower($type ?? '') === 'date-picker')
 <x-datetime-picker
-    without-timezone
-    without-time
-    :label="$label"
-    :placeholder="$placeholder"
-    :disabled="$disabled"
-    :readonly="$readonly"
-    :min="data_get($options, 'min', false)"
-    :max="data_get($options, 'max', false)"
-    wire:model.defer="fields.{{ $key }}"
+	without-timezone
+	without-time
+	:label="$label"
+	:placeholder="$placeholder"
+	:disabled="$disabled"
+	:readonly="$readonly"
+	:min="data_get($options, 'min', false)"
+	:max="data_get($options, 'max', false)"
+	:parse-format="data_get($options, 'format.parse', 'YYYY-MM-DD')"
+	wire:model.defer="fields.{{ $key }}"
 />
 @endif
--- resources/views/includes/form-field/datetime-picker.blade.php
+++ resources/views/includes/form-field/datetime-picker.blade.php
@@ -1,12 +1,13 @@
 @if (strtolower($type ?? '') === 'datetime-picker')
-    <x-datetime-picker
-        without-timezone
-        :label="$label"
-        :placeholder="$placeholder"
-        :disabled="$disabled"
-        :readonly="$readonly"
-        :min="data_get($options, 'min', false)"
-        :max="data_get($options, 'max', false)"
-        wire:model.defer="fields.{{ $key }}"
-    />
+<x-datetime-picker
+    without-timezone
+    :label="$label"
+    :placeholder="$placeholder"
+	:disabled="$disabled"
+    :readonly="$readonly"
+    :min="data_get($options, 'min', false)"
+	:max="data_get($options, 'max', false)"
+    :parse-format="data_get($options, 'format.parse', 'YYYY-MM-DD HH:mm:ss')"
+    wire:model.defer="fields.{{ $key }}"
+/>
 @endif
