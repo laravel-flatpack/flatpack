8f14ba8d0345ac0df86bad45bc01162aa6d1a37b
Fausto Quaggia <fausto.quaggia@gmail.com>
2023-03-03 22:56:27 +0100

    test: form component

--- tests/Livewire/FormTest.php
+++ tests/Livewire/FormTest.php
@@ -3,6 +3,7 @@
 use Livewire\Livewire;
 use Flatpack\Http\Livewire\Form;
 use Flatpack\Tests\Models\Post;
+use Flatpack\Tests\Models\Tag;
 
 it('redirects back to index list', function () {
     Livewire::test(Form::class, [
@@ -30,12 +31,43 @@ it('displays an error if action does not exist', function () {
     ]);
 });
 
-it('creates a new post entry', function () {
-    $post = new Post();
+it('throw error if fields validation fails', function () {
     Livewire::test(Form::class, [
         'entity' => 'posts',
         'model' => Post::class,
-        'entry' => $post,
+        'entry' => new Post(),
+        'formType' => 'create',
+        'composition' => [
+            'header' => [
+                'title' => [
+                    'type' => 'editable',
+                    'rules' => 'required|string'
+                ]
+            ],
+            'toolbar' => [
+                'save' => [
+                    'label' => 'Save',
+                    'action' => 'save',
+                    'redirect' => true
+                ]
+            ],
+            'main' => [
+                'body' => [
+                    'type' => 'textarea',
+                    'rules' => 'required|string'
+                ]
+            ]
+        ]
+    ])
+    ->call('action', 'save')
+    ->assertHasErrors();
+});
+
+it('creates a new post entry', function () {
+    Livewire::test(Form::class, [
+        'entity' => 'posts',
+        'model' => Post::class,
+        'entry' => new Post(),
         'formType' => 'create',
         'composition' => [
             'header' => [
@@ -55,7 +87,7 @@ it('creates a new post entry', function () {
             ],
             'main' => [
                 'body' => [
-                    'type' => 'block-editor',
+                    'type' => 'textarea',
                     'rules' => 'required'
                 ]
             ]
@@ -74,7 +106,6 @@ it('creates a new post entry', function () {
 });
 
 it('updates an existing post entry', function () {
-    
     $post = Post::create([
         'title' => 'Testing title'
     ]);
@@ -103,7 +134,7 @@ it('updates an existing post entry', function () {
             ],
             'main' => [
                 'body' => [
-                    'type' => 'block-editor',
+                    'type' => 'textarea',
                     'rules' => 'required'
                 ]
             ]
@@ -116,4 +147,181 @@ it('updates an existing post entry', function () {
     ->call('action', 'save');
 
     $this->assertFalse(Post::whereTitle('Testing title')->exists());
+    $this->assertTrue(Post::whereTitle('Lorem ipsum')->exists());
+});
+
+it('handles form field components state', function () {
+    $post = Post::create([
+        'title' => 'Testing title'
+    ]);
+
+    $this->assertTrue(Post::whereTitle('Testing title')->exists());
+
+    Livewire::test(Form::class, [
+        'entity' => 'posts',
+        'model' => Post::class,
+        'entry' => $post,
+        'formType' => 'edit',
+        'composition' => [
+            'header' => [
+                'title' => [
+                    'type' => 'editable',
+                    'rules' => 'required|string'
+                ]
+            ],
+            'toolbar' => [
+                'save' => [
+                    'label' => 'Save',
+                    'action' => 'save'
+                ]
+            ],
+            'main' => [
+                'body' => [
+                    'type' => 'block-editor',
+                    'rules' => 'required'
+                ],
+                'image' => [
+                    'type' => 'image-upload'
+                ]
+            ]
+        ]
+    ])
+    ->call('saveEditorState', 'body', ['foo' => 'bar'])
+    ->assertSet('fields', [
+        'title' => 'Testing title', 
+        'body' => json_encode(['foo' => 'bar']),
+        'image' => null
+    ])
+    ->call('saveImageUploaderState', 'image', ['img1', 'img2', 'img3'])
+    ->assertSet('fields', [
+        'title' => 'Testing title', 
+        'body' => json_encode(['foo' => 'bar']),
+        'image' => ['img1', 'img2', 'img3']
+    ]);
+});
+
+it('creates a new related entry', function () {
+    
+    $post = Post::create([
+        'title' => 'Lorem ipsum'
+    ]);
+
+    $component = Livewire::test(Form::class, [
+        'entity' => 'posts',
+        'model' => Post::class,
+        'entry' => $post,
+        'formType' => 'edit',
+        'composition' => [
+            'header' => [
+                'title' => [
+                    'type' => 'editable',
+                    'size' => 'large',
+                    'placeholder' => 'Your post title...',
+                    'rules' => 'required|string'
+                ]
+            ],
+            'toolbar' => [
+                'save' => [
+                    'label' => 'Save',
+                    'action' => 'save'
+                ]
+            ],
+            'main' => [
+                'body' => [
+                    'type' => 'block-editor',
+                    'rules' => 'required'
+                ],
+                'tags' => [
+                    'type' => 'tag-input',
+                    'placeholder' => 'Write tags...',
+                    'relation' => [
+                        'name' => 'tags',
+                        'display'=> 'name',
+                        'create' => true
+                    ]
+                ],
+            ]
+        ]
+    ]);
+
+    $component
+        ->set('fields', [
+            'title' => 'Lorem ipsum',
+            'body' => 'Lorem ipsum dolor sit amet.'
+        ])
+        ->call('createRelatedEntity', 'tags', 'testing-new-tag')
+        ->assertEmitted('flatpack-form:related-entity-created:tags', 1)
+        ->call('createRelatedEntity', 'tags', 'another-tag')
+        ->assertEmitted('flatpack-form:related-entity-created:tags', 2);
+
+    $this->assertTrue(Tag::whereName('testing-new-tag')->exists());
+    $this->assertTrue(Tag::whereName('another-tag')->exists());
+
+    $component
+        ->call('saveTagInputState', 'tags', '1,2')
+        ->call('action', 'save');
+    
+    $tags = $post->refresh()->tags->pluck('name');
+    
+    $this->assertTrue($tags->contains('testing-new-tag'));
+    $this->assertTrue($tags->contains('another-tag'));
+});
+
+
+it('handles form failures and error messages', function () {
+    
+    $post = Post::create([
+        'title' => 'Lorem ipsum'
+    ]);
+
+    $component = Livewire::test(Form::class, [
+        'entity' => 'posts',
+        'model' => Post::class,
+        'entry' => $post,
+        'formType' => 'edit',
+        'composition' => [
+            'header' => [
+                'title' => [
+                    'type' => 'editable',
+                    'rules' => 'required|string'
+                ]
+            ],
+            'toolbar' => [
+                'save' => [
+                    'label' => 'Save',
+                    'action' => 'save'
+                ]
+            ],
+            'main' => [
+                'body' => [
+                    'type' => 'block-editor',
+                    'rules' => 'required'
+                ],
+                'tags' => [
+                    'type' => 'tag-input',
+                    'relation' => [
+                        'name' => 'tags',
+                        'display'=> 'not-existing',
+                        'create' => true
+                    ]
+                ],
+            ]
+        ]
+    ]);
+
+    $component
+        ->call('createRelatedEntity', 'tags', 'foo')
+        ->assertEmitted('notify', [
+            'type' => 'error',
+            'message' => 'Failed to create new tags',
+            'errors' => []
+        ]);
+
+    $component
+        ->call('showImageUploaderError', 'Error Message', [])
+        ->assertEmitted('notify', [
+            'type' => 'error',
+            'message' => 'Error Message',
+            'errors' => []
+        ]);
 });
