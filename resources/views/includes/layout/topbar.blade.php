115c91a3d6f7fc0ae3fb60d39e2bcf1a0a43ae25
Fausto Quaggia <fausto.quaggia@gmail.com>
2023-04-02 20:27:26 +0200

    fix: sidebar navigation

--- resources/views/includes/layout/topbar.blade.php
+++ resources/views/includes/layout/topbar.blade.php
@@ -1,9 +1,11 @@
 <div class="top-bar-wrapper">
     <div class="flex items-center justify-start gap-5 text-gray-300">
-        <button @click="navbar = !navbar" aria-label="{{ __('Toggle Navbar') }}">
-            <div x-show="!navbar"><x-icon name="login" style="solid" class="w-6 h-6" /></div>
-            <div x-show="navbar"><x-icon name="menu" style="outline" class="w-6 h-6" /></div>
-        </button>
+        <div class="block xl:hidden">
+            <button @click="navbar = !navbar" aria-label="{{ __('Toggle Navbar') }}">
+                <div x-show="!navbar"><x-icon name="login" style="solid" class="w-6 h-6" /></div>
+                <div x-show="navbar"><x-icon name="menu" style="outline" class="w-6 h-6" /></div>
+            </button>
+        </div>
     </div>
     <div class="flex gap-4 text-white">
         @auth
@@ -13,13 +15,17 @@
                 <span>{{ $user->name }}</span>
                 <x-icon name="chevron-down" style="outline" class="w-3 h-3" />
             </button>
-            <div x-show="open" @click.away="open = false" class="absolute right-0 w-40 mt-2 text-left origin-top-right bg-white rounded-md shadow-lg top-8 ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+            <div x-show="open" @click.away="open = false" class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                 <div class="py-1" role="none">
-                    <form method="POST" action="{{ route('flatpack.logout') }}" class="mb-0">
+                    <a class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" href="https://github.com/laravel-flatpack/flatpack" target="_blank" role="menuitem" tabindex="-1">{{ __('Support') }}</a>
+                    <a class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100" href="https://github.com/laravel-flatpack/flatpack/blob/main/LICENSE.md" target="_blank" role="menuitem" tabindex="-1">{{ __('License') }}</a>
+                </div>
+                <div class="py-1" role="none">
+                    <form method="POST" action="{{ route('flatpack.logout') }}" class="mb-0" role="none">
                     @csrf
                         <a href="{{ route('flatpack.logout') }}"
                             onclick="event.preventDefault(); this.closest('form').submit();"
-                            class="block px-4 py-2 text-sm text-gray-700"
+                            class="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100"
                             role="menuitem"
                             tabindex="-1">
                             <span>{{ __('Log Out') }}</span>
