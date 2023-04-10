99a889472271f7f4cc7d6697d006bef5be012cb4
Fausto Quaggia <fausto.quaggia@gmail.com>
2023-04-10 17:18:06 +0200

    fix: toolbar

--- resources/views/components/tools/toolbar.blade.php
+++ resources/views/components/tools/toolbar.blade.php
@@ -31,20 +31,29 @@
 
         @if ($component->searchIsEnabled() && $component->searchVisibilityIsEnabled())
             <div class="flex rounded-md shadow-sm">
-                <input
-                    wire:model{{ $component->getSearchOptions() }}="{{ $component->getTableName() }}.search"
+                @php
+                    $tableName = $component->getTableName();
+                    $hasSearch = $component->hasSearch();
+                @endphp
+                <x-input 
+                    wire:model="{{ $tableName }}.search"
                     placeholder="{{ __('Search') }}"
-                    type="text"
-                    class="block w-full border-gray-300 rounded-md shadow-sm transition duration-150 ease-in-out sm:text-sm sm:leading-5 dark:bg-gray-700 dark:text-white dark:border-gray-600 @if ($component->hasSearch()) rounded-none rounded-l-md focus:ring-0 focus:border-gray-300 @else focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md @endif"
-                />
-
-                @if ($component->hasSearch())
-                    <span wire:click.prevent="clearSearch" class="inline-flex items-center px-3 text-gray-500 bg-gray-50 rounded-r-md border border-l-0 border-gray-300 cursor-pointer sm:text-sm dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </span>
-                @endif
+                >
+                    <x-slot name="append">
+                        @if ($hasSearch)
+                            <div class="absolute inset-y-0 right-0 flex items-center p-0.5">
+                                <x-button
+                                    wire:click.prevent="clearSearch"
+                                    class="h-full rounded-r-md"
+                                    icon="x"
+                                    flat
+                                    squared
+                                />
+                            </div>
+                        @endif
+                    </x-slot>
+                </x-input>
+                
             </div>
         @endif
 
@@ -237,7 +246,11 @@
                                         class="inline-flex items-center px-2 py-1 disabled:opacity-50 disabled:cursor-wait"
                                     >
                                         <input
-                                            class="text-indigo-600 transition duration-150 ease-in-out border-gray-300 rounded shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-900 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600 dark:focus:bg-gray-600 disabled:opacity-50 disabled:cursor-wait"
+                                            class="form-checkbox rounded transition ease-in-out duration-100 
+                                                border-secondary-300 text-primary-600 focus:ring-primary-600 focus:border-primary-400
+                                                dark:border-secondary-500 dark:checked:border-secondary-600 dark:focus:ring-secondary-600
+                                                dark:focus:border-secondary-500 dark:bg-secondary-600 dark:text-secondary-600
+                                                dark:focus:ring-offset-secondary-800"
                                             @if($component->allDefaultVisibleColumnsAreSelected())
                                                 checked
                                                 wire:click="deselectAllColumns"
@@ -260,7 +273,11 @@
                                                 class="inline-flex items-center px-2 py-1 disabled:opacity-50 disabled:cursor-wait"
                                             >
                                                 <input
-                                                    class="text-indigo-600 rounded border-gray-300 shadow-sm transition duration-150 ease-in-out focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:bg-gray-900 dark:text-white dark:border-gray-600 dark:hover:bg-gray-600 dark:focus:bg-gray-600 disabled:opacity-50 disabled:cursor-wait"
+                                                    class="form-checkbox rounded transition ease-in-out duration-100 
+                                                        border-secondary-300 text-primary-600 focus:ring-primary-600 focus:border-primary-400
+                                                        dark:border-secondary-500 dark:checked:border-secondary-600 dark:focus:ring-secondary-600
+                                                        dark:focus:border-secondary-500 dark:bg-secondary-600 dark:text-secondary-600
+                                                        dark:focus:ring-offset-secondary-800"
                                                     wire:model="selectedColumns"
                                                     wire:target="selectedColumns"
                                                     wire:loading.attr="disabled"
