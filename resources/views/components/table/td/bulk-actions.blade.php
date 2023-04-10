a60b221d838d1059ae25ffba2a91d5ca76db8f89
Fausto Quaggia <fausto.quaggia@gmail.com>
2023-04-10 17:18:14 +0200

    fix: bulk actions

--- resources/views/components/table/td/bulk-actions.blade.php
+++ resources/views/components/table/td/bulk-actions.blade.php
@@ -7,12 +7,15 @@
     @endphp
 
     <x-livewire-tables::table.td.plain>
-        <div class="inline-flex rounded-md shadow-sm">
-            <x-checkbox 
-                wire:model="selected"
-                wire:loading.attr.delay="disabled"
-                value="{{ $row->{$this->getPrimaryKey()} }}"
-            />
-        </div>
+        <input 
+            wire:model="selected"
+            type="checkbox" 
+            value="{{ $row->{$this->getPrimaryKey()} }}"
+            class="form-checkbox rounded transition ease-in-out duration-100 
+                    border-secondary-300 text-primary-600 focus:ring-primary-600 focus:border-primary-400
+                    dark:border-secondary-500 dark:checked:border-secondary-600 dark:focus:ring-secondary-600
+                    dark:focus:border-secondary-500 dark:bg-secondary-600 dark:text-secondary-600
+                    dark:focus:ring-offset-secondary-800"
+        />
     </x-livewire-tables::table.td.plain>
 @endif
