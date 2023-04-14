3514ba7c55cea9da20f640cd673e820cffb7ce9e
Fausto Quaggia <fausto.quaggia@gmail.com>
2023-04-14 16:03:34 +0200

    test: improve coverage

--- src/Http/Middleware/FlatpackMiddleware.php
+++ src/Http/Middleware/FlatpackMiddleware.php
@@ -51,15 +51,15 @@ class FlatpackMiddleware
         return $next($request);
     }
 
-    protected function authorize(Request $request)
+    /**
+     * Authorize Http Request for the current user.
+     * On local env, all users are allowed.
+     *
+     * @return bool
+     */
+    protected function authorize(Request $request): bool
     {
-        if (app()->environment('local')) {
-            return true;
-        }
-
-        $user = $request->user();
-
-        return $user->isFlatpackAdmin();
+        return app()->environment('local') || $request->user()->isFlatpackAdmin();
     }
 
     /**
