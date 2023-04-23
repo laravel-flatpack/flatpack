b0ac860b0843f84833bf543fe5aecbe68313d4ac
Fausto Quaggia <fausto.quaggia@gmail.com>
2023-04-23 22:59:19 +0200

    feat: image column

--- resources/views/includes/columns/image.blade.php
+++ resources/views/includes/columns/image.blade.php
@@ -1,6 +1,6 @@
 <img
     src="{{ $path }}" 
-    class="rounded h-10 w-16 object-cover mx-0 -my-2"
+    class="rounded h-10 w-12 object-cover mx-0 -my-2"
     
     {!! count($attributes) ? $column->arrayToAttributes($attributes) : '' !!}
 />
