ea1ecd4e763b7da9b4e06a997b73a65b6a4eaa3d
Fausto Quaggia <fausto.quaggia@gmail.com>
2023-04-26 20:34:55 +0200

    feat: top menu toggle

--- resources/views/includes/layout/topbar.blade.php
+++ resources/views/includes/layout/topbar.blade.php
@@ -1,12 +1,17 @@
 <div class="top-bar-wrapper">
     <div class="container text-gray-300 mx-auto max-w-[1400px]">
         <div class="flex justify-between items-center gap-5 w-full">
-            <div class="flex items-center justify-start gap-4 w-1/2">
-                <div class="flex items-center justify-start gap-4 xl:hidden">
-                    <button @click="navbar = !navbar" aria-label="{{ __('Toggle Navbar') }}">
-                        <x-icon name="menu" style="solid" class="w-5 h-5" />
-                    </button>
-                </div>
+            <div class="flex items-center justify-start gap-2 w-1/2">
+                <button 
+                    @click="navbar = !navbar"
+                    aria-label="{{ __('Toggle Navbar') }}"
+                    x-bind:class="{ 'is-open': !navbar }"
+                    class="menu-toggle"
+                >
+                    <x-icon name="menu" style="solid" class="w-5 h-5 menu-show" />
+                    <x-icon name="x" style="solid" class="w-5 h-5 menu-hide" />
+                </button>
+                
                 <livewire:flatpack.search-box />
             </div>
             <div class="flex items-center justify-end gap-4 w-1/2">
