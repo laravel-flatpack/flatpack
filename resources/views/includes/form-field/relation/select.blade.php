c2dd4dfa0db73bc43d038fa2a05ba7ac63ed95f9
Fausto Quaggia <fausto.quaggia@gmail.com>
2023-05-01 01:00:20 +0200

    fix: async data route

--- resources/views/includes/form-field/relation/select.blade.php
+++ resources/views/includes/form-field/relation/select.blade.php
@@ -4,7 +4,10 @@
         wire:model.defer="{{ $binding }}.{{ $key }}"
         :placeholder="$placeholder"
         :label="$label"
-        :async-data="route('flatpack.api.suggestions', $entity)"
+        :async-data="route('flatpack.api.suggestions', [
+            'entity' => Flatpack::entityName($relationshipModel),
+            'display' => $getFieldOption('relation.display', 'name')
+        ])"
         option-label="display"
         option-value="value"
     />
