9a65681bf9f7472f9a4d1056780b6726df38e448
Fausto Quaggia <fausto.quaggia@gmail.com>
2023-05-07 19:56:30 +0200

    chore: remove comments

--- resources/js/flatpack.js
+++ resources/js/flatpack.js
@@ -1,4 +1,3 @@
-// Flatpack version: 1.0.0
 import createFlatpack from "./utility/flatpack";
 import { initEventListeners } from "./events";
 
--- resources/js/form.js
+++ resources/js/form.js
@@ -1,7 +1,5 @@
-// Flatpack version: 1.0.0
 import { taginput } from "./components";
 
-// Add components to Flatpack global object
 window.Flatpack = {
   ...window.Flatpack,
   taginput,
