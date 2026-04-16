837137cefb116f8122d82ad9a9d2c9b6d9bfb1d7
Fausto Quaggia <fausto.quaggia@gmail.com>
2026-04-16 12:21:12 +0200

    fix: sidebar width

--- resources/js/layouts/flatpack-layout.tsx
+++ resources/js/layouts/flatpack-layout.tsx
@@ -34,7 +34,7 @@ export default function FlatpackLayout({
         <SidebarProvider
             style={
                 {
-                    '--sidebar-width': 'calc(var(--spacing) * 72)',
+                    '--sidebar-width': 'calc(var(--spacing) * 64)',
                     '--header-height': 'calc(var(--spacing) * 12)',
                 } as React.CSSProperties
             }
