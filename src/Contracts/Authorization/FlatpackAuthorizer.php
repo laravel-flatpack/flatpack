c331b913d738b10017187ec5b48e9eda129913cd
Fausto Quaggia <fausto.quaggia@gmail.com>
2026-04-17 20:48:30 +0200

    feat: authorizer interface

--- src/Contracts/Authorization/FlatpackAuthorizer.php
+++ src/Contracts/Authorization/FlatpackAuthorizer.php
@@ -10,11 +10,21 @@ interface FlatpackAuthorizer
 {
     /**
      * Whether the user may access the Flatpack admin area at all.
+     * 
+     * @param Authenticatable|null $user The user to check.
+     * @return bool Whether the user may access the Flatpack admin area at all.
      */
     public function canAccessPanel(?Authenticatable $user): bool;
 
     /**
-     * Whether the user may perform a policy ability on a model class (optional instance for "update"/"delete").
+     * Whether the user may perform a policy ability on a model class.
+     * Optional Model instance for "update"/"delete".
+     * 
+     * @param Authenticatable $user The user to check.
+     * @param string $ability The ability to check, e.g. "create", "update", "delete".
+     * @param string $modelClass The model class to check.
+     * @param object|null $model The model instance to check.
+     * @return bool Whether the user may perform the action on the model.
      */
     public function allows(
         ?Authenticatable $user,
