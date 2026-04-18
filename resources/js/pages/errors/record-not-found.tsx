808bf642871dc37e7e9eebb35d686d431120b9bf
Fausto Quaggia <fausto.quaggia@gmail.com>
2026-04-18 12:57:39 +0200

    feat: error page

---
+++ resources/js/pages/errors/record-not-found.tsx
@@ -0,0 +1,48 @@
+import { Head, Link } from '@inertiajs/react';
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { route } from 'ziggy-js';
+import { ReactElement } from 'react';
+import FlatpackLayout from '@/layouts/flatpack-layout';
+import { BoxIcon, CircleAlertIcon, PackageOpenIcon } from 'lucide-react';
+
+export type FlatpackRecordNotFoundPageProps = {
+    entity: string;
+    entityName: string;
+};
+
+export default function FlatpackRecordNotFoundPage({
+    entity,
+    entityName,
+}: FlatpackRecordNotFoundPageProps) {
+    const title = `Record not found`;
+    return (
+        <>
+            <Head title={title} />
+            <div className="flex flex-col items-center justify-center py-6 px-2 md:p-10">
+                <div className="w-full max-w-md">
+                    <Card size="sm" className="mx-auto w-full max-w-md">
+                        <CardHeader>
+                            <CardTitle>{title}</CardTitle>
+                            <CardDescription>The {entityName} you are looking for does not exist.</CardDescription>
+                            <CardContent className="flex flex-col items-center gap-2 !px-0">
+                                <p className="my-10">
+                                    We could not find that {entityName}. It may have been removed, or you might not have
+                                    permission to view it.
+                                </p>
+                            </CardContent>
+                            <CardFooter className="flex-col gap-2 !px-0">
+                                <Button variant="default" className="w-full" asChild>
+                                    <Link href={route('flatpack.entities.index', { entity })}>Back to {entityName} list</Link>
+                                </Button>
+                            </CardFooter>
+                        </CardHeader>
+                    </Card>
+                </div>
+            </div>
+        </>
+    );
+}
+FlatpackRecordNotFoundPage.layout = (page: ReactElement<FlatpackRecordNotFoundPageProps>) => (
+    <FlatpackLayout>{page}</FlatpackLayout>
+);
